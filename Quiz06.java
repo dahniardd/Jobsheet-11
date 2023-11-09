e023a57b19cc7a8b23fb0dac6fe6f285c377402a
dahniardd <dahniardvn09@gmail.com>
2023-11-09 09:23:39 +0700

    Percobaan 4 modifikasi

--- Quiz06.java
+++ Quiz06.java
@@ -14,7 +14,14 @@ public class Quiz06 {
             System.out.print("Tebak angka (1-10): ");
             int answer = input06.nextInt();
             input06.nextLine();
-            success = (answer==number);
+            if(answer < number){
+                System.out.println("Maaf, angka yang anda masukkan terlalu kecil. Coba lagi!");
+            } else if(answer > number){
+                System.out.println("Maaf, angka yang anda masukkan terlalu besar. Coba lagi!");
+            } else{
+                success = true;
+                System.out.println("Selamat! Tebakan anda benar!");
+            }
         }while(!success);
         System.out.print("Apakah Anda ingin mengulang permainan (Y/y)?");
         menu = input06.nextLine().charAt(0);
