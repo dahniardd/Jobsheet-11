6b0e37f12fba5eceaf77aca7d5ac44fc8a711cb6
dahniardd <dahniardvn09@gmail.com>
2023-11-09 10:58:40 +0700

    Tugas Individu 2

---
+++ TugasIndividu206.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class TugasIndividu206 {
+    public static void main (String[]Args){
+
+        Scanner input06 = new Scanner(System.in); 
+        
+        System.out.print ("Masukkan nilai N (minimal 5):"); 
+        int N = input06.nextInt();
+
+        if (N < 5){
+        }
+        for (int i = N; i >= 1; i--){
+            for (int j = 1; j<= 1; j++) { 
+                System.out.print("*"); 
+            }
+            System.out.println();
+}
+}
+}
