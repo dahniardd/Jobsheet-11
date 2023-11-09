6cbd7d04dd604514f457b7fb4edaef23f9ab935c
dahniardd <dahniardvn09@gmail.com>
2023-11-09 08:47:54 +0700

    Percobaan 3

---
+++ Triangle06.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Triangle06 {
+    public static void main (String[]Args){
+        Scanner input06 = new Scanner(System.in);
+        
+        System.out.print("Masukkan nilai N = ");
+        int N = input06.nextInt();
+        int i = 0;
+        while(i<=N){
+            int j = 0;
+            while(j<i){
+                System.out.print("*");
+                j++;
+            }
+        i++;
+}
+    }
+}
