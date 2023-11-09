414a61574f427a0c3999a00f9310f0febb98a51f
dahniardd <dahniardvn09@gmail.com>
2023-11-09 08:06:05 +0700

    Percobaan 1

---
+++ Stars06.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Stars06{
+    public static void main(String[] Args){
+        Scanner input06 = new Scanner(System.in);
+        
+        System.out.print("Masukkan nilai N = ");
+        int N = input06.nextInt();
+
+        for(int i=1; i<=N;i++){
+            System.out.print("*");
+        }
+    }
+}
